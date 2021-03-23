a73e1caa17495a90bef59830ce2b36f12f99803d
Pavel <recyclesordie94@gmail.com>
2021-03-23 21:12:59 +0300

    путь самурая 1.0
    социальная сеть
    пробую реакт
    пробую webstorm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import test from "./google.js";
+import React from 'react';
 
-function App() {
+const App = () =>  {
+    return (
+        <div>
+            <newElem
+            <h1>ogogo</h1>
+          <Alena />
+        <div><Header /></div>
+        <Technologys />
+
+        </div>
+    );
+}
+
+
+const Alena = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <img src="" alt=""/>
+      <h1>ALENA</h1>
+
+      </div>
+  )
+}
+const Technologys = () => {
+  return (
+    <div></div>
   );
+}
+
+const newElem = () => {
+    return (
+        <p>hello new Elem</p>
+    );
+}
+
+const Header = () =>  {
+  return (
+      <div>
+        <h2 className="test">promos1 </h2>
+
+        <a href="#">Homes</a> <br/>
+        <a href="#">News</a> <br/>
+        <a href="#">Messages</a>
+      </div>
+  );
+}
+
+
+const list = () => {
+    return (
+        <ul>
+            <li>css</li>
+            <li>HTML</li>
+            <li>JavaScript</li>
+            <li>nodeJs</li>
+            <li>engine</li>
+
+        </ul>
+    );
 }
 
 export default App;
