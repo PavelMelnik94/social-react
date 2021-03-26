a9079b758dafb63e61b20e1404aa6cec9fd000d7
Pavel <recyclesordie94@gmail.com>
2021-03-26 21:19:10 +0300

    props для Диалогов
    компонента диалога и сообщения

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog} >
+            <NavLink  activeClassName={s.active} to={'/dialogs/' + props.id}> {props.name} </NavLink>
+        </div>
+    );
+}
+
+
+const Messages = (props) => {
+    return (
+        <div className={s.dialog}> {props.text}</div>
+    );
+}
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <div className={s.dialog + ' ' + s.active}> диалог</div>
-                <div className={s.dialog}> диалог1</div>
-                <div className={s.dialog}> диалог2</div>
-                <div className={s.dialog}> диалог3</div>
+                <DialogItem name='Dimych' id='1'/>
+                <DialogItem name='Petya' id='2'/>
+                <DialogItem name='Vasia' id='3'/>
+                <DialogItem name='Alexandr' id='4'/>
+                <DialogItem name='Pavel' id='5'/>
+                <DialogItem name='Egor' id='6'/>
+                <DialogItem name='Daniil' id='7'/>
+                <DialogItem name='Oleg' id='8'/>
+                <DialogItem name='Antony' id='9'/>
+                <DialogItem name='Alexey' id='10'/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}> hi </div>
-                <div className={s.message}> how is yo</div>
-                <div className={s.message}> dsdasdasd</div>
+                <Messages text='Hellow, orld!' />
+                <Messages text='hi' />
+                <Messages text='how are you?' />
+                <Messages text='+453 4534 45786' />
+                <Messages text='im fine' />
+                <Messages text='goodbay' />
+                <Messages text='another day of sunrise' />
+                <Messages text='fish text' />
+                <Messages text='lorem 5 words' />
+                <Messages text='thanks!!!' />
             </div>
         </div>
     )
